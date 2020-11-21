334928d07e5c8b1a186e8e16c95b545d33188982
yuma <yuuma1403@gmail.com>
2020-11-21 19:34:32 +0900

    add shop card

---
+++ components/ShopCard.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <template v-if="$vuetify.breakpoint.xs">
+      <v-card v-for="shop in shops" :key="shop" class="mb-5">
+        <v-img height="200" :src="shop.img_src"></v-img>
+        <v-card-title class="font-weight-bold pb-0">
+          {{ shop.name }}
+          <v-btn icon class="text-left">
+            <v-icon>mdi-bookmark-outline</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <v-icon color="red">mdi-map-marker</v-icon>
+          <span class="grey--text text--darken-2 text-caption">
+            {{ shop.address.substr(13) }}
+          </span>
+          <v-row no-gutters class="mt-2 mb-1">
+            <v-col cols="auto">
+              <div style="width: 40px">
+                <v-img
+                  src="/tabelog.svg"
+                  height="30"
+                  width="30"
+                  class="mr-2"
+                  contain
+                ></v-img>
+              </div>
+            </v-col>
+            <v-col cols="auto" align-self="center">
+              <stars-rating
+                :rating="shop.tabelog_rating"
+                star-size="20"
+              ></stars-rating>
+            </v-col>
+          </v-row>
+          <v-row no-gutters class="mb-2">
+            <v-col cols="auto" align-self="center">
+              <div style="width: 35px; margin-left: 5px">
+                <v-img
+                  src="/google.svg"
+                  height="20"
+                  width="20"
+                  class="mr-2"
+                  contain
+                ></v-img>
+              </div>
+            </v-col>
+            <v-col cols="auto">
+              <stars-rating
+                :rating="shop.google_rating"
+                star-size="20"
+              ></stars-rating>
+            </v-col>
+          </v-row>
+          <v-row no-gutters>
+            <v-col cols="auto" align-self="center" class="mr-2">
+              <span class="font-weight-bold">おすすめ度</span>
+            </v-col>
+            <v-col cols="auto">
+              <stars-rating
+                :rating="shop.total_rating"
+                star-size="20"
+              ></stars-rating>
+            </v-col>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </template>
+    <template v-else>
+      <v-row>
+        <v-col v-for="shop in shops" :key="shop" cols="12" xl="6">
+          <v-card class="mb-5">
+            <div class="d-flex flex-row">
+              <v-avatar size="200" tile>
+                <v-img :src="shop.img_src"></v-img>
+              </v-avatar>
+              <div class="align-self-center mb-2">
+                <v-card-title class="font-weight-bold pb-2 pt-0">
+                  {{ shop.name }}
+                  <v-btn icon class="text-left">
+                    <v-icon>mdi-bookmark-outline</v-icon>
+                  </v-btn>
+                </v-card-title>
+                <v-card-text>
+                  <v-icon color="red">mdi-map-marker</v-icon>
+                  <span class="grey--text text--darken-2">{{
+                    shop.address
+                  }}</span>
+                </v-card-text>
+                <div class="d-flex flex-row">
+                  <div class="d-flex flex-row ml-3">
+                    <v-img
+                      src="/tabelog.svg"
+                      height="30"
+                      width="30"
+                      class="mr-2"
+                      contain
+                    ></v-img>
+                    <stars-rating
+                      :rating="shop.tabelog_rating"
+                      star-size="25"
+                    ></stars-rating>
+                  </div>
+                  <div class="d-flex flex-row ml-5">
+                    <v-img
+                      src="/google.svg"
+                      height="20"
+                      width="20"
+                      class="mr-2"
+                      contain
+                    ></v-img>
+                    <stars-rating
+                      :rating="shop.google_rating"
+                      star-size="25"
+                    ></stars-rating>
+                  </div>
+                </div>
+                <div class="d-flex flex-row mt-3 ml-3">
+                  <span class="font-weight-bold mr-2">おすすめ度</span>
+                  <stars-rating
+                    :rating="shop.total_rating"
+                    star-size="25"
+                  ></stars-rating>
+                </div>
+              </div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
+  </div>
+</template>
+
+<script>
+import StarsRating from '@/components/StarRating'
+export default {
+  components: { StarsRating },
+  props: {
+    shops: {
+      required: true,
+    },
+  },
+  name: 'ShopCard',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ components/StarRating.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="star-rating">
+    <div v-for="(star, index) in stars" :key="index" class="star-container">
+      <svg
+        class="star-svg"
+        :style="[
+          { fill: `url(#gradient${star.raw})` },
+          { width: starSize },
+          { height: starSize },
+        ]"
+      >
+        <polygon :points="getStarPoints" style="fill-rule: nonzero" />
+        <defs>
+          <!--
+            id has to be unique to each star fullness(dynamic offset) - it indicates fullness above
+          -->
+          <linearGradient :id="`gradient${star.raw}`">
+            <stop
+              id="stop1"
+              :offset="star.percent"
+              stop-opacity="1"
+              :stop-color="getFullFillColor(star)"
+            ></stop>
+            <stop
+              id="stop2"
+              :offset="star.percent"
+              stop-opacity="0"
+              :stop-color="getFullFillColor(star)"
+            ></stop>
+            <stop
+              id="stop3"
+              :offset="star.percent"
+              stop-opacity="1"
+              :stop-color="styleEmptyStarColor"
+            ></stop>
+            <stop
+              id="stop4"
+              offset="100%"
+              stop-opacity="1"
+              :stop-color="styleEmptyStarColor"
+            ></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+    <div v-if="isIndicatorActive" class="indicator ml-1">
+      <span class="font-weight-bold" style="color: #e10001">{{ rating }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarsRating',
+  components: {},
+  directives: {},
+  props: {
+    rating: {
+      type: Number,
+      default: 4.0,
+    },
+    starSize: {
+      type: Number,
+      default: 25,
+    },
+    isIndicatorActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  data() {
+    return {
+      stars: [],
+      emptyStar: 0,
+      fullStar: 1,
+      totalStars: 5,
+      styleEmptyStarColor: '#E1E1E1',
+      styleFullStarColor: '#FF6A00',
+    }
+  },
+  computed: {
+    getStarPoints() {
+      const centerX = this.starSize / 2
+      const centerY = this.starSize / 2
+
+      const innerCircleArms = 5 // a 5 arms star
+
+      const innerRadius = this.starSize / innerCircleArms
+      const innerOuterRadiusRatio = 2.5 // Unique value - determines fatness/sharpness of star
+      const outerRadius = innerRadius * innerOuterRadiusRatio
+
+      return this.calcStarPoints(
+        centerX,
+        centerY,
+        innerCircleArms,
+        innerRadius,
+        outerRadius
+      )
+    },
+  },
+  created() {
+    this.initStars()
+    this.setStars()
+  },
+  methods: {
+    calcStarPoints(
+      centerX,
+      centerY,
+      innerCircleArms,
+      innerRadius,
+      outerRadius
+    ) {
+      const angle = Math.PI / innerCircleArms
+      const angleOffsetToCenterStar = 60
+
+      const totalArms = innerCircleArms * 2
+      let points = ''
+      for (let i = 0; i < totalArms; i++) {
+        const isEvenIndex = i % 2 === 0
+        const r = isEvenIndex ? outerRadius : innerRadius
+        const currX =
+          centerX + Math.cos(i * angle + angleOffsetToCenterStar) * r
+        const currY =
+          centerY + Math.sin(i * angle + angleOffsetToCenterStar) * r
+        points += currX + ',' + currY + ' '
+      }
+      return points
+    },
+    initStars() {
+      for (let i = 0; i < this.totalStars; i++) {
+        this.stars.push({
+          raw: this.emptyStar,
+          percent: this.emptyStar + '%',
+        })
+      }
+    },
+    setStars() {
+      let fullStarsCounter = Math.floor(this.rating)
+      for (let i = 0; i < this.stars.length; i++) {
+        if (fullStarsCounter !== 0) {
+          this.stars[i].raw = this.fullStar
+          this.stars[i].percent = this.calcStarFullness(this.stars[i])
+          fullStarsCounter--
+        } else {
+          const surplus = Math.round((this.rating % 1) * 10) / 10 // Support just one decimal
+          const roundedOneDecimalPoint = Math.round(surplus * 10) / 10
+          this.stars[i].raw = roundedOneDecimalPoint
+          return (this.stars[i].percent = this.calcStarFullness(this.stars[i]))
+        }
+      }
+    },
+    getFullFillColor(starData) {
+      return starData.raw !== this.emptyStar
+        ? this.styleFullStarColor
+        : this.styleEmptyStarColor
+    },
+    calcStarFullness(starData) {
+      const starFullnessPercent = starData.raw * 100 + '%'
+      return starFullnessPercent
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.star-rating {
+  display: flex;
+  align-items: center;
+
+  .star-container {
+    display: flex;
+
+    .star-svg {
+    }
+  }
+
+  .indicator {
+  }
+
+  .star-container:not(:last-child) {
+    //margin-right: 5px;
+  }
+}
+</style>
