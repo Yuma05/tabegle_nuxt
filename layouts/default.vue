0745544a3fa5d0f0246f892cfaa6c008691739dd
yuma <yuuma1403@gmail.com>
2020-11-23 16:03:04 +0900

    add menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,41 @@
   <v-app dark>
     <v-app-bar color="#F5A002" fixed app>
       <v-toolbar-title
-        class="text-center white--text mx-auto font-weight-bold text-h4"
-        >Tabegle</v-toolbar-title
+        class="white--text font-weight-bold text-h5 text-sm-h4 ml-5"
+      >
+        <nuxt-link to="/" style="text-decoration: none; color: white"
+          >Tabegle</nuxt-link
+        >
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        :small="$vuetify.breakpoint.xs"
+        color="white"
+        outlined
+        class="mr-2"
+        @click="$router.push('/keep')"
+      >
+        <v-icon>mdi-bookmark</v-icon>keep</v-btn
+      >
+      <v-btn
+        v-if="$store.getters.loggedin"
+        :small="$vuetify.breakpoint.xs"
+        color="white"
+        outlined
+        class="mr-2"
+        @click="logout"
+      >
+        <v-icon>mdi-logout</v-icon>logout</v-btn
+      >
+      <v-btn
+        v-else
+        :small="$vuetify.breakpoint.xs"
+        color="white"
+        outlined
+        class="mr-2"
+        @click="$router.push('/login')"
+      >
+        <v-icon>mdi-login</v-icon>login</v-btn
       >
     </v-app-bar>
     <v-content>
@@ -15,16 +48,36 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data() {
     return {
-      clipped: false,
-      fixed: false,
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      isLogin: false,
     }
+  },
+  mounted() {
+    if (this.$cookies.get('key') == null) {
+      this.$store.commit('changeLoginStatus', false)
+    } else {
+      this.$store.commit('changeLoginStatus', true)
+    }
+  },
+  methods: {
+    logout() {
+      const key = this.$cookies.get('key')
+      const headers = {
+        'X-CSRFToken': this.$cookies.get('csrftoken'),
+        Authorization: key,
+      }
+      axios
+        .post('/api/dj-rest-auth/logout/', null, { headers })
+        .then((res) => {
+          this.$cookies.remove('key')
+          this.$store.commit('changeLoginStatus', false)
+          this.$router.push('/')
+        })
+        .catch((e) => console.log(e.response))
+    },
   },
 }
 </script>
