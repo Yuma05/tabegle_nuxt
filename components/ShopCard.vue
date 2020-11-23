975b43a079ff732e9a54086a4589a056fc1d0a62
yuma <yuuma1403@gmail.com>
2020-11-23 16:06:16 +0900

    add link,sort methods,keep methods

--- components/ShopCard.vue
+++ components/ShopCard.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
+    <v-tabs color="orange" centered class="mb-2">
+      <v-tabs-slider color="orange"></v-tabs-slider>
+      <v-tab @click="sortTabelog">食べログ</v-tab>
+      <v-tab @click="sortGoogle">google</v-tab>
+      <v-tab @click="sortTotal">おすすめ</v-tab>
+    </v-tabs>
     <template v-if="$vuetify.breakpoint.xs">
       <v-card v-for="shop in shops" :key="shop.id" class="mb-5">
         <v-img height="200" :src="shop.img_src"></v-img>
         <v-card-title class="font-weight-bold pb-0">
-          {{ shop.name }}
-          <v-btn icon class="text-left">
+          <a :href="shop.url" style="text-decoration: none; color: black">
+            {{ shop.name }}
+          </a>
+          <v-btn
+            v-if="userShopIds.includes(shop.id)"
+            icon
+            class="text-left"
+            @click="$emit('delete', shop.id)"
+          >
+            <v-icon color="red">mdi-bookmark</v-icon>
+          </v-btn>
+          <v-btn v-else icon class="text-left" @click="$emit('add', shop.id)">
             <v-icon>mdi-bookmark-outline</v-icon>
           </v-btn>
         </v-card-title>
         <v-card-text>
           <v-icon color="red">mdi-map-marker</v-icon>
           <span class="grey--text text--darken-2 text-caption">
-            {{ shop.address.substr(13) }}
+            <a
+              :href="'https://www.google.com/search?q=' + shop.name"
+              style="color: gray"
+              >{{ shop.address.substr(13) }}</a
+            >
           </span>
           <v-row no-gutters class="mt-2 mb-1">
             <v-col cols="auto">
@@ -29,7 +49,7 @@
             <v-col cols="auto" align-self="center">
               <stars-rating
                 :rating="shop.tabelog_rating"
-                star-size="20"
+                :star-size="20"
               ></stars-rating>
             </v-col>
           </v-row>
@@ -48,7 +68,7 @@
             <v-col cols="auto">
               <stars-rating
                 :rating="shop.google_rating"
-                star-size="20"
+                :star-size="20"
               ></stars-rating>
             </v-col>
           </v-row>
@@ -59,7 +79,7 @@
             <v-col cols="auto">
               <stars-rating
                 :rating="shop.total_rating"
-                star-size="20"
+                :star-size="20"
               ></stars-rating>
             </v-col>
           </v-row>
@@ -76,16 +96,38 @@
               </v-avatar>
               <div class="align-self-center mb-2">
                 <v-card-title class="font-weight-bold pb-2 pt-0">
-                  {{ shop.name }}
-                  <v-btn icon class="text-left">
+                  <a
+                    :href="shop.url"
+                    style="text-decoration: none; color: black"
+                  >
+                    {{ shop.name }}
+                  </a>
+                  <v-btn
+                    v-if="userShopIds.includes(shop.id)"
+                    icon
+                    class="text-left"
+                    @click="$emit('delete', shop.id)"
+                  >
+                    <v-icon color="red">mdi-bookmark</v-icon>
+                  </v-btn>
+                  <v-btn
+                    v-else
+                    icon
+                    class="text-left"
+                    @click="$emit('add', shop.id)"
+                  >
                     <v-icon>mdi-bookmark-outline</v-icon>
                   </v-btn>
                 </v-card-title>
                 <v-card-text>
                   <v-icon color="red">mdi-map-marker</v-icon>
-                  <span class="grey--text text--darken-2">{{
-                    shop.address.substr(13)
-                  }}</span>
+                  <span class="grey--text text--darken-2"
+                    ><a
+                      :href="'https://www.google.com/search?q=' + shop.name"
+                      style="color: gray"
+                      >{{ shop.address.substr(13) }}</a
+                    ></span
+                  >
                 </v-card-text>
                 <div class="d-flex flex-row">
                   <div class="d-flex flex-row ml-3">
@@ -137,12 +179,45 @@ export default {
   components: { StarsRating },
   props: {
     shops: {
+      type: Array,
       required: true,
     },
+    userShopIds: {
+      type: Array,
+      default: () => [],
+    },
   },
-  name: 'ShopCard',
   data() {
     return {}
+  },
+  methods: {
+    sortTabelog() {
+      this.shops.sort((a, b) => {
+        if (a.tabelog_rating < b.tabelog_rating) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+    },
+    sortGoogle() {
+      this.shops.sort((a, b) => {
+        if (a.google_rating < b.google_rating) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+    },
+    sortTotal() {
+      this.shops.sort((a, b) => {
+        if (a.total_rating < b.total_rating) {
+          return 1
+        } else {
+          return -1
+        }
+      })
+    },
   },
 }
 </script>
