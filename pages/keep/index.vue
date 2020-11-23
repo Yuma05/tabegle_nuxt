c9edda2e56ddb697a14fec0b2fbd7647d1b54817
yuma <yuuma1403@gmail.com>
2020-11-23 16:04:15 +0900

    add new methods

--- pages/keep/index.vue
+++ pages/keep/index.vue
@@ -4,7 +4,13 @@
       v-if="isLoading"
       :messages="['店舗情報を取得しています']"
     ></loading-box>
-    <shop-card v-else-if="shops.length !== 0" :shops="shops"></shop-card>
+    <shop-card
+      v-else-if="shops.length !== 0"
+      :shops="shops"
+      :user-shop-ids="userShopIds"
+      @delete="deleteUserShop"
+      @add="addUserShop"
+    ></shop-card>
     <div v-else class="text-center grey--text text--darken-3">
       <div class="text-center mt-15 mb-5">気になるお店をキープできます！</div>
       <v-btn color="orange" dark @click="$router.push('/')">
@@ -22,13 +28,14 @@ export default {
   name: 'index',
   data() {
     return {
-      isLoading: false,
+      isLoading: true,
       shops: [],
+      userShopIds: [],
     }
   },
   components: { ShopCard, LoadingBox },
   mounted() {
-    // this.getUserShops()
+    this.getUserShops()
   },
   methods: {
     async getUserShops() {
@@ -43,9 +50,38 @@ export default {
       }
       await axios
         .get('/api/user-shop/', { headers })
-        .then((res) => (this.shops = res.data.shops))
+        .then((res) => {
+          this.shops = res.data.shops
+          this.userShopIds = res.data.shops.map((item) => item.id)
+        })
         .catch((e) => console.log(e))
         .finally((this.isLoading = false))
+    },
+    deleteUserShop(id) {
+      const key = this.$cookies.get('key')
+      const headers = {
+        'X-CSRFToken': this.$cookies.get('csrftoken'),
+        Authorization: key,
+      }
+      axios
+        .delete('/api/user-shop/' + id, { headers })
+        .then((res) => {
+          this.userShopIds = res.data.shops.map((item) => item.id)
+        })
+        .catch((e) => console.log(e))
+    },
+    addUserShop(id) {
+      const key = this.$cookies.get('key')
+      const headers = {
+        'X-CSRFToken': this.$cookies.get('csrftoken'),
+        Authorization: key,
+      }
+      axios
+        .put('/api/user-shop/' + id, null, { headers })
+        .then((res) => {
+          this.userShopIds = res.data.shops.map((item) => item.id)
+        })
+        .catch((e) => console.log(e.response))
     },
   },
 }
